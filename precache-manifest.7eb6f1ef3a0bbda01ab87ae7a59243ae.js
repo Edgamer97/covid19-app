7612d93720bb1730b04ef975bb2b46f8feacfb72
Edgar Garcete <edgamer97@gmail.com>
2020-05-25 18:08:04 -0400

    Updates

---
+++ precache-manifest.7eb6f1ef3a0bbda01ab87ae7a59243ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7339a43913e59db37794438bbd0ddf74",
+    "url": "/covid19-app/index.html"
+  },
+  {
+    "revision": "ce00581e047c244ee535",
+    "url": "/covid19-app/static/css/main.34444971.chunk.css"
+  },
+  {
+    "revision": "f427ee15f935efd966bd",
+    "url": "/covid19-app/static/js/2.a2ee4a79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-app/static/js/2.a2ee4a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce00581e047c244ee535",
+    "url": "/covid19-app/static/js/main.c3d507cd.chunk.js"
+  },
+  {
+    "revision": "8fb5219e47f1bf69ff16",
+    "url": "/covid19-app/static/js/runtime-main.a99c342c.js"
+  },
+  {
+    "revision": "75697806822c47679a4d3c5be4384e72",
+    "url": "/covid19-app/static/media/coronavirus.75697806.png"
+  },
+  {
+    "revision": "923fc5effa275965629281c11541d650",
+    "url": "/covid19-app/static/media/error.923fc5ef.png"
+  },
+  {
+    "revision": "942e46b818619f4abef6e6c95a5b9790",
+    "url": "/covid19-app/static/media/overlay.942e46b8.png"
+  },
+  {
+    "revision": "3551210e409b90428182b865f85a16df",
+    "url": "/covid19-app/static/media/tenor.3551210e.gif"
+  }
+]);
